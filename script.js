b4e8aadee68e97d4fe0120e2c9eff8b04050d762
kim <Kimvz19@users.noreply.github.com>
2025-03-12 11:48:45 +0100

    adding js function voor achternaam uppercase eerste letter

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("hi");
+
+
+let achternaamInput = document.querySelector('.achternaam');
+
+achternaamInput.addEventListener('input', function() {
+    if (achternaamInput.value.length > 0) {
+        achternaamInput.value = achternaamInput.value.charAt(0).toUpperCase() + achternaamInput.value.slice(1);
+    }
+});
